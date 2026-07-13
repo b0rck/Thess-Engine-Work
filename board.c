2f444f674becba169f1dcd77cb7f2aaf00baba63
Brock Albright <brockablright@gmail.com>
2026-07-12 21:02:34 -0700

    start board and vision structs

--- board.c
+++ board.c
@@ -3,4 +3,11 @@
 
 #include "board.h"
 #include <stdio.h>
-// Testing Git!
+
+typedef struct {
+    Piece squares[8][8];
+} Board;
+
+typedef struct {
+    bool squares[8][8]
+} Vision;
--- board.h
+++ board.h
@@ -4,9 +4,49 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <stdbool.h>
+
+#define BOARD_SIZE 8
+
+typedef enum {
+    PIECE_NONE,
+    PIECE_PAWN,
+    PIECE_KNIGHT,
+    PIECE_BISHOP,
+    PIECE_ROOK,
+    PIECE_QUEEN,
+    PIECE_KING
+} PieceType;
+
+typedef enum {
+    COLOR_NONE,
+    COLOR_WHITE,
+    COLOR_BLACK
+} PieceColor;
+
+typedef struct {
+    PieceType;
+    PieceColor;
+} Piece;
+
 typedef struct
 {
-    int squares[8][8];//board data goes here
+    Piece squares[BOARD_SIZE][BOARD_SIZE];
 } Board;
+
+typedef struct {
+    bool squares[BOARD_SIZE][BOARD_SIZE];
+} Vision;
+
+void board_init_empty(Board *board);
+void board_print(const Board *board, const Vision *vision);
+
+void vision_clear(Vision *vision);
+void vision_calculate_rook(
+    const Board *board,
+    Vision *vision,
+    int row,
+    int colum
+);
 
 #endif
